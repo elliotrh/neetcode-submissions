a9b5601d9fd0384b363ca4fa5c248319cc3fbc91
elliotrh <elliotrh@gmail.com>
2026-07-14 23:57:03 -0700

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int trap(int[] height) {
+    //brute force solution
+        int area = 0;
+        for(int i = 1; i < height.length - 1; i++){
+            int left_max = 0; int right_max = 0;
+            for(int j = 0; j < i; j++){
+                if(left_max < height[j]){
+                    left_max = height[j];
+                }
+            }
+            for(int k = i + 1; k < height.length; k++){
+                if(right_max < height[k]){
+                    right_max = height[k];
+                }
+            }
+            
+            area += Math.max(Math.min(left_max, right_max) - height[i], 0);
+        }
+        return area;
+    }
+}
